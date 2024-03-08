2c23645ab51182476d55eac384f9ebe8ace46102
ifloresr7 <ifloresr@uoc.edu>
2024-03-08 13:01:58 +0100

    Se crea la clase ViewIndex

---
+++ Senderos/src/viewIndex.java
@@ -0,0 +1,11 @@
+public class viewIndex {
+    public String gestionEscursion(){
+        return null;
+    }
+    public String gestionSocio(){
+        return null;
+    }
+    public String gestionInscripciones(){
+        return null;
+    }
+}
