fe5d713927f4932e152114741bd6cdb37951d177
mromerotor <mromerotor@uoc.edu>
2024-03-20 11:37:49 +0100

    Update ExcursionModel
    Se han añadido los métodos para añadir, modificar, mostrar y eliminar excursiones.
    OJO, hay tres excepciones:
    1. En modificacionExcursion, que revisa si el indice de la excursion a modificar esta en rango o no,
    2. En eliminarExcursion, misma comprobacion que anterior.
    Esto es asi porque no se de que manera se pueden implementar en Controlador. Modificar segun necesidades.

--- ObjectData/src/ObjectData_app/ObjectData_model/ExcursionModel.java
+++ ObjectData/src/ObjectData_app/ObjectData_model/ExcursionModel.java
@@ -1,5 +1,6 @@
 package ObjectData_app.ObjectData_model;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ExcursionModel{
@@ -51,8 +52,6 @@ public class ExcursionModel{
         this.precioInscripcion = precioInscripcion;
     }
 
-
-
     //Metodos
     public String toString() {
         return "Excursion{" +
@@ -63,4 +62,41 @@ public class ExcursionModel{
                 ", precioInscripcion=" + precioInscripcion +
                 '}';
     }
+    
+    public static void añadirExcursion(Datos datos, ExcursionModel nuevaExcursion) {
+        datos.getExcursiones().add(nuevaExcursion);
+        System.out.println("Excursión añadida correctamente.");
+    }
+
+    public static void modificarExcursion(Datos datos, int indice, ExcursionModel excursionModificada) {
+        ArrayList<ExcursionModel> excursiones = datos.getExcursiones();
+        if (indice >= 0 && indice < excursiones.size()) {
+            excursiones.set(indice, excursionModificada);
+            System.out.println("Excursión modificada correctamente.");
+        } else {
+            System.out.println("El índice proporcionado está fuera de rango.");
+        }
+    }
+
+    public static void mostrarExcursiones(Datos datos) {
+        ArrayList<ExcursionModel> excursiones = datos.getExcursiones();
+        if (excursiones.isEmpty()) {
+            System.out.println("No hay excursiones para mostrar.");
+        } else {
+            for (ExcursionModel excursion : excursiones) {
+                System.out.println(excursion); 
+            }
+        }
+    }
+
+    public static void eliminarExcursion(Datos datos, int indice) {
+        ArrayList<ExcursionModel> excursiones = datos.getExcursiones();
+        if (indice >= 0 && indice < excursiones.size()) {
+            excursiones.remove(indice);
+            System.out.println("Excursión eliminada correctamente.");
+        } else {
+            System.out.println("El índice proporcionado está fuera de rango.");
+        }
+    }
 }
+
