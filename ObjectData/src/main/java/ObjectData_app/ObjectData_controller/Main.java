f52cad1db564939e97d0076a59db8ba47c7d0987
ifloresr7 <151829949+ifloresr7@users.noreply.github.com>
2024-04-03 18:06:04 +0200

    .

--- ObjectData/src/main/java/ObjectData_app/ObjectData_controller/Main.java
+++ ObjectData/src/main/java/ObjectData_app/ObjectData_controller/Main.java
@@ -3,10 +3,45 @@
 //la llamada al metodo menuInicio del controlador.
 package ObjectData_app.ObjectData_controller;
 import ObjectData_app.ObjectData_model.Datos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        // Se inicializa la base de BBDD virtual.
+        // Datos de conexión
+        String hostname = "62.72.37.1";
+        String port = "3306";
+        String database = "nombre_de_tu_base_de_datos";
+        String username = "u450392368_ObjectData";
+        String password = "WmAb[>gM1]";
+
+        // URL de conexión
+        String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database;
+
+        try {
+            // Cargar el controlador JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establecer conexión
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // Verificar si la conexión fue exitosa
+            if (connection != null) {
+                System.out.println("Conexión exitosa a la base de datos.");
+                // Puedes realizar operaciones con la base de datos aquí
+                // Por ejemplo, ejecutar consultas, insertar datos, etc.
+            } else {
+                System.out.println("Fallo al conectar a la base de datos.");
+            }
+
+            // Cerrar la conexión
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error al cargar el controlador JDBC: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error de SQL: " + e.getMessage());
+        }
         Datos BBDD = new Datos();
         AppController.cargarDatosController(BBDD);
         AppController.inicio(BBDD);
