c4f79e7c5fa381354fe137ead11e1e5ee7324d25
ifloresr7 <151829949+ifloresr7@users.noreply.github.com>
2024-04-05 15:32:56 +0200

    .

--- ObjectData/src/main/java/ObjectData_app/ObjectData_view/MensajeControllerView.java
+++ ObjectData/src/main/java/ObjectData_app/ObjectData_view/MensajeControllerView.java
@@ -6,20 +6,20 @@ public class MensajeControllerView {
     // Atributos
     static Scanner teclado = new Scanner(System.in);
     static String p = "\u001B[0m";
-    static String p2 = "\033[36m";
     static String error = "\033[31m";
+    static String out = "\u001B[35m";
 
     // Este metodo se usa para devolver respuestas generadas por la aplicación
     public void respuestaControllerView(String respuesta) {
         System.out.println(respuesta);
-        System.out.println(p2 + "\nPulse una tecla para continuar..." + p);
+        System.out.println(out + "\nPulse ENTER para continuar..." + p);
         teclado.nextLine();
     }
 
     // Este metodo se usa para devolver errores generados en la aplicación
     public void excepcionesControllerView(String respuesta) {
         System.out.println(error + "¡ERROR! - " + respuesta + p);
-        System.out.println("\nPulse una tecla para continuar...");
+        System.out.println(out + "\nPulse ENTER para continuar..." + p);
         teclado.nextLine();
     }
 }
