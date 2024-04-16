18da227f8ff9c15a185e3e716490f93e3e518887
mariaarovirac <162487640+mariaarovirac@users.noreply.github.com>
2024-04-16 10:15:44 +0200

    Socio Estandar y federado DAO.

--- ObjectData/src/main/java/ObjectData_app/ObjectData_model/ObjectData_DAO/Implementacion/SocioEstandarDAOImpl.java
+++ ObjectData/src/main/java/ObjectData_app/ObjectData_model/ObjectData_DAO/Implementacion/SocioEstandarDAOImpl.java
@@ -16,12 +16,14 @@ public class SocioEstandarDAOImpl implements SocioEstandarDAO {
     //Con este metodo lo que hacemos es una sentencia SQL para obtener todos los socios estandar almacenados en la base de datos.
     @Override
     public ArrayList<SocioEstandarModel> obtenerTodosSocioEstandar() throws SQLException {
+        Connection con = ConexionBD.obtenerConexion();
+        PreparedStatement pst = null;
         ArrayList<SocioEstandarModel> socios = new ArrayList<>();
         SeguroModel seguro;
-        try(
-            Connection con = ConexionBD.obtenerConexion();
-            PreparedStatement pst = con.prepareStatement("SELECT * FROM socioEstandar");
-            ResultSet respuestaBD = pst.executeQuery()) {
+        try{
+            con.setAutoCommit(false);
+            pst = con.prepareStatement("SELECT * FROM socioEstandar");
+            ResultSet respuestaBD = pst.executeQuery();
             while (respuestaBD.next()) {
                 seguro = new SeguroModel(TipoSeguro.valueOf(respuestaBD.getString("seguro")));
                 SocioEstandarModel socio = new SocioEstandarModel(
@@ -32,20 +34,36 @@ public class SocioEstandarDAOImpl implements SocioEstandarDAO {
                 );
                 socios.add(socio);
             }
+            con.commit();
+
         } catch (SQLException e) {
-            throw new SQLException("Fallo en la consulta SQL al obtener todos los socios estándar.");
+        if (con != null) {
+            con.rollback();
         }
-        return socios;
+        throw new SQLException("Fallo en la consulta SQL al obtener todos los socios estándar.");
+    } finally {
+        if (pst != null) {
+            pst.close();
+        }
+        if (con != null) {
+            con.setAutoCommit(true);
+            con.close();
+        }
     }
+    return socios;
+    }
+    
     //Con este metodo se obtiene el socio estandar que coincida con el numero de socio.
     @Override
     public SocioEstandarModel obtenerSocioEstandarPorNumeroSocio(int numeroSocio) throws SQLException {
+        Connection con = ConexionBD.obtenerConexion();
+        PreparedStatement pst = null;
+        con.setAutoCommit(false);
         SocioEstandarModel socio = null;
-        try (
-            Connection con = ConexionBD.obtenerConexion();
-            PreparedStatement pst = con.prepareStatement("SELECT * FROM socioEstandar WHERE numeroSocio=?")
-        ) {
+        try {
+            pst = con.prepareStatement("SELECT * FROM socioEstandar WHERE numeroSocio=?");
             pst.setInt(1, numeroSocio);
+    
             try (ResultSet respuestaBD = pst.executeQuery()) {
                 if (respuestaBD.next()) {
                     SeguroModel seguro = new SeguroModel(TipoSeguro.valueOf(respuestaBD.getString("seguro")));
@@ -57,51 +75,107 @@ public class SocioEstandarDAOImpl implements SocioEstandarDAO {
                     );
                 }
             }
+            con.commit();
+
         } catch (SQLException e) {
+            if (con != null) {
+                con.rollback();
+            }
             throw new SQLException("Fallo en la consulta SQL al obtener el socio estándar por número de socio: " + numeroSocio);
+        } finally {
+            if (pst != null) {
+                pst.close();
+            }
+            if (con != null) {
+                con.setAutoCommit(true);
+                con.close();
+            }
         }
         return socio;
-    }
+    } 
     
     @Override
     public void crearSocioEstandar(SocioEstandarModel socio) throws SQLException {
-        try (Connection con = ConexionBD.obtenerConexion();
-             PreparedStatement pst = con.prepareStatement("INSERT INTO socioEstandar (numeroSocio, nombre, NIF, seguro) VALUES (?, ?, ?, ?)")
-        ) {
+        Connection con = ConexionBD.obtenerConexion();
+        PreparedStatement pst = null;
+        con.setAutoCommit(false);
+        try {
+            pst = con.prepareStatement("INSERT INTO socioEstandar (numeroSocio, nombre, NIF, seguro) VALUES (?, ?, ?, ?)");
             pst.setInt(1, socio.getNumeroSocio());
             pst.setString(2, socio.getNombre());
             pst.setString(3, socio.getNIF());
             pst.setString(4, socio.getSeguro().getTipo().toString());
             pst.executeUpdate();
+            con.commit();
+
         } catch (SQLException e) {
+            if (con != null) {
+                con.rollback();
+            }
             throw new SQLException("Fallo en la consulta SQL al crear el socio estándar.");
+        } finally {
+            if (pst != null) {
+                pst.close();
+            }
+            if (con != null) {
+                con.setAutoCommit(true);
+                con.close();
+            }
         }
     }
     //Metodo usado para actualizar el socio, en este caso se usa porque podemos cambiar el seguro del tipo de socio estandar.
     @Override
     public void actualizarSocioEstandar(SocioEstandarModel socio) throws SQLException {
-        try (Connection con = ConexionBD.obtenerConexion();
-             PreparedStatement pst = con.prepareStatement("UPDATE socioEstandar SET nombre=?, NIF=?, seguro=? WHERE numeroSocio=?")
-        ) {
+        Connection con = ConexionBD.obtenerConexion(); // Se obtiene una conexión a la base de datos
+        PreparedStatement pst = null; //De decrara la variable que almacenara la consula.
+        con.setAutoCommit(false); //Desactiva el AutoCommit de la BBDD, basicamente para hacer el rollback
+        try{
+            pst = con.prepareStatement("UPDATE socioEstandar SET nombre=?, NIF=?, seguro=? WHERE numeroSocio=?");
             pst.setString(1, socio.getNombre());
             pst.setString(2, socio.getNIF());
             pst.setString(3, socio.getSeguro().getTipo().toString());
             pst.setInt(4, socio.getNumeroSocio());
             pst.executeUpdate();
+            con.commit(); // Si todo va bien, confirmamos la transacción
         } catch (SQLException e) {
+            if (con != null) {
+                con.rollback();
+            }
             throw new SQLException("Fallo en la consulta SQL al actualizar el socio estándar.");
+        } finally {
+            if (pst != null) {
+                pst.close();
+            }
+            if (con != null) {
+                con.setAutoCommit(true);
+                con.close();
+            }
         }
     }
     //Metodo para eliminar el socio estandar.
     @Override
     public void eliminarSocioEstandar(int numeroSocio) throws SQLException {
-        try (Connection con = ConexionBD.obtenerConexion();
-             PreparedStatement pst = con.prepareStatement("DELETE FROM socioEstandar WHERE numeroSocio=?")
-        ) {
+        Connection con = ConexionBD.obtenerConexion(); // Se obtiene una conexión a la base de datos
+        PreparedStatement pst = null; // Se declara la variable que almacenará la consulta
+        con.setAutoCommit(false); // Desactiva el AutoCommit de la BBDD, básicamente para hacer el rollback
+    try {
+            pst = con.prepareStatement("DELETE FROM socioEstandar WHERE numeroSocio=?");
             pst.setInt(1, numeroSocio);
             pst.executeUpdate();
+            con.commit();
         } catch (SQLException e) {
+            if (con != null) {
+                con.rollback();
+            }
             throw new SQLException("Fallo en la consulta SQL al eliminar el socio estándar.");
+        } finally {
+            if (pst != null) {
+                pst.close();
+            }
+            if (con != null) {
+                con.setAutoCommit(true);
+                con.close();
+            }
         }
     }
 }
--- ObjectData/src/main/java/ObjectData_app/ObjectData_model/ObjectData_DAO/Implementacion/SocioFederadoDAOImpl.java
+++ ObjectData/src/main/java/ObjectData_app/ObjectData_model/ObjectData_DAO/Implementacion/SocioFederadoDAOImpl.java
@@ -16,12 +16,13 @@ public class SocioFederadoDAOImpl implements SocioFederadoDAO {
     //Con este metodo lo que hacemos es una sentencia SQL para obtener todos los socios estandar almacenados en la base de datos.
     @Override
     public ArrayList<SocioFederadoModel> obtenerTodosSocioFederado() throws SQLException {
+        Connection con = ConexionBD.obtenerConexion(); // Se obtiene una conexión a la base de datos
+        PreparedStatement pst = null; //De decrara la variable que almacenara la consula.
+        con.setAutoCommit(false); //Desactiva el AutoCommit de la BBDD, basicamente para hacer el rollback
         ArrayList<SocioFederadoModel> socios = new ArrayList<>(); // Se crea una lista para almacenar los socios federados encontrados
-        try (
-            Connection con = ConexionBD.obtenerConexion(); // Se obtiene una conexión a la base de datos
-            PreparedStatement pst = con.prepareStatement("SELECT * FROM socioFederado"); // Se prepara una consulta SQL para seleccionar todos los socios federados
-            ResultSet respuestaBD = pst.executeQuery() // Se ejecuta la consulta y se almacena el resultado en un ResultSet
-        ) {
+        try {
+            pst = con.prepareStatement("SELECT * FROM socioFederado"); // Se prepara una consulta SQL para seleccionar todos los socios federados
+            ResultSet respuestaBD = pst.executeQuery(); // Se ejecuta la consulta y se almacena el resultado en un ResultSet
             while (respuestaBD.next()) { // Mientras haya resultados en el ResultSet...
                 String codigoFederacion = respuestaBD.getString("codigoFederacion"); // Se obtiene el código de federación del resultado
                 FederacionDAO federacionDAO = new FederacionDAOImpl(); // Se crea una instancia de FederacionDAOImpl
@@ -34,19 +35,32 @@ public class SocioFederadoDAOImpl implements SocioFederadoDAO {
                 );
                 socios.add(socio); // Se agrega el objeto SocioFederadoModel a la lista de socios federados
             }
+            con.commit(); // Si todo va bien, confirmamos la transacción
         } catch (Exception e) { // Si ocurre alguna excepción durante el proceso...
+            if (con != null) {
+                con.rollback(); // Revertir transacción en caso de error
+            }
             throw new SQLException("Fallo en la consulta SQL al obtener todos los socios federados."); // Se lanza una excepción indicando el error
+        } finally {
+            if (pst != null) {
+                pst.close();
+            }
+            if (con != null) {
+                con.setAutoCommit(true);
+                con.close();
+            }
         }
         return socios; // Se devuelve la lista de socios federados encontrados
     }
     //Con este metodo se obtiene el socio estandar que coincida con el numero de socio.
     @Override
     public SocioFederadoModel obtenerPorNumeroSocio(int numeroSocio) throws SQLException {
+        Connection con = ConexionBD.obtenerConexion(); // Se obtiene una conexión a la base de datos
+        PreparedStatement pst = null; //De decrara la variable que almacenara la consula.
+        con.setAutoCommit(false); //Desactiva el AutoCommit de la BBDD, basicamente para hacer el rollback
         SocioFederadoModel socio = null;
-        try (
-            Connection con = ConexionBD.obtenerConexion();
-            PreparedStatement pst = con.prepareStatement("SELECT * FROM socioFederado WHERE numeroSocio=?")
-        ) {
+        try {
+            pst = con.prepareStatement("SELECT * FROM socioFederado WHERE numeroSocio=?");
             pst.setInt(1, numeroSocio);
             try (ResultSet respuestaBD = pst.executeQuery()) {
                 if (respuestaBD.next()) {
@@ -61,36 +75,75 @@ public class SocioFederadoDAOImpl implements SocioFederadoDAO {
                     );
                 }
             }
+            con.commit(); // Si todo va bien, confirmamos la transacción
         } catch (Exception e) {
+            if (con != null) {
+                con.rollback();  // Si algo sale mal, revertimos la transacción
+            }
             throw new SQLException("Fallo en la consulta SQL al obtener el socio con número de socio: " + numeroSocio);
+        } finally {
+            if (pst != null) {
+                pst.close(); //Se cierra el statement.
+            }
+            if (con != null) {
+                con.setAutoCommit(true);  // Revertimos a la configuración por defecto
+                con.close(); //Se cierra la conexión de la base de datos.                
+            }
         }
         return socio;
     }
     //Metodo para crear un socio federado.
     @Override
     public void crearSocioFederado(SocioFederadoModel socio) throws SQLException {
-        try (Connection con = ConexionBD.obtenerConexion();
-            PreparedStatement pst = con.prepareStatement("INSERT INTO socioFederado (numeroSocio, nombre, NIF, codigoFederacion) VALUES (?, ?, ?, ?)")
-        ){
+        Connection con = ConexionBD.obtenerConexion(); // Se obtiene una conexión a la base de datos
+        PreparedStatement pst = null; //De decrara la variable que almacenara la consula.
+        con.setAutoCommit(false); //Desactiva el AutoCommit de la BBDD, basicamente para hacer el rollback
+        try {
+            pst = con.prepareStatement("INSERT INTO socioFederado (numeroSocio, nombre, NIF, codigoFederacion) VALUES (?, ?, ?, ?)");
             pst.setInt(1, socio.getNumeroSocio());
             pst.setString(2, socio.getNombre());
             pst.setString(3, socio.getNIF());
             pst.setString(4, socio.getFederacion().getCodigo().toString());
             pst.executeUpdate();
+            con.commit(); // Si todo va bien, confirmamos la transacción
         } catch (SQLException e) {
+            if (con != null) {
+                con.rollback();
+            }
             throw new SQLException("Fallo en la consulta SQL al crear un socio federado.");
+        }finally {
+            if (pst != null) {
+                pst.close();
+            }
+            if (con != null) {
+                con.setAutoCommit(true);
+                con.close();
+            }
         }
     }
     //Metodo para eliminar al socio Federado.
     @Override
     public void eliminarSocioFederado(int numeroSocio) throws SQLException {
-        try (Connection con = ConexionBD.obtenerConexion();
-            PreparedStatement pst = con.prepareStatement("DELETE FROM socioFederado WHERE numeroSocio=?")
-        ) {
+        Connection con = ConexionBD.obtenerConexion(); // Se obtiene una conexión a la base de datos
+        PreparedStatement pst = null; //De decrara la variable que almacenara la consula.
+        con.setAutoCommit(false); //Desactiva el AutoCommit de la BBDD, basicamente para hacer el rollback
+        try {
+            pst = con.prepareStatement("DELETE FROM socioFederado WHERE numeroSocio=?");
             pst.setInt(1, numeroSocio);
             pst.executeUpdate();
         } catch (SQLException e) {
+            if (con != null) {
+                con.rollback();
+            }
             throw new SQLException("Fallo en la consulta SQL al eliminar el socio federado.");
+        }finally {
+            if (pst != null) {
+                pst.close(); //Se cierra el statement.
+            }
+            if (con != null) {
+                con.setAutoCommit(true);  // Revertimos a la configuración por defecto
+                con.close(); //Se cierra la conexión de la base de datos.                
+            }
         }
     }
 }
